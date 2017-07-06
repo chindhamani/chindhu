88b7494aa11e3c787847e9f9a18705089244841f
chindhamani <chindhamani1997@gmail.com>
2017-07-06 11:25:41 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ public class Numuber
+ {
+   
+   public static void main(String args[])
+   {
+     
+     Scanner s=new Scanner(System.in);
+     int a;
+     a=s.nextInt();
+     if(a>0)
+     {
+       
+       System.out.println("The number is postive"+a);
+       }
+else if(a==0)
+{
+    
+  System.out.println("The number is zero");
+  }
+    else 
+      
+    // end of if-else
+        System.out.println("The number is negative"+a);
+  }
+  }
+  
