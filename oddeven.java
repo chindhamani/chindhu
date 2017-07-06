039001398a2858e68ff70cbf640c8b7bc03cf950
chindhamani <chindhamani1997@gmail.com>
2017-07-06 12:08:21 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ public class OddEven
+ {
+   
+   public static void main(String args[])
+   {
+     
+     Scanner s=new Scanner(System.in);
+     int a;
+     a=s.nextInt();
+     if(a%2==0)
+     {
+       
+       System.out.println("The number is even");
+       }
+  
+    else 
+      
+  
+        System.out.println("The number is odd");
+  }
+  }
